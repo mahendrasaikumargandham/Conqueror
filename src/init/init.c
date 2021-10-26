13d7f941a34a6b3c15585a460cc38deb0d225dc0
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2021-10-26 10:00:53 +0530

    Updated `init.c` file

--- src/init/init.c
+++ src/init/init.c
@@ -1,1 +1,18 @@
-printf('init.c');
+#include <syscall.h>
+#include <fcntl.h>
+#include <lib.h>
+
+int main() 
+{
+	char *msg = "Conqueror_1.0 Initializing";
+	sleep_sec(1);
+	
+	str_print(msg);
+	sleep_sec(1);
+	execute_process("/bin/lash");
+	while(1)
+	{
+		sleep_sec(1);
+	}
+	return 0;
+}
