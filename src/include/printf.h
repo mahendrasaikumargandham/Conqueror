000bdd182efad89655eef5ad5a9c7da2581f1e3e
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2021-10-26 09:57:33 +0530

    Update printf.h

--- src/include/printf.h
+++ src/include/printf.h
@@ -1,1 +1,10 @@
-<h1>printf</h1>
+#ifndef __TFP_PRINTF__
+#define __TFP_PRINTF__
+#include <stdarg.h>
+#define printf tfp_printf
+#define sprintf tfp_sprintf
+#endif
+void init_printf(void* putp,void (*putf) (void*,char));
+void tfp_printf(char *fmt, ...);
+void tfp_sprintf(char* s,char *fmt, ...);
+void tfp_format(void* putp,void (*putf) (void*,char),char *fmt, va_list va);
