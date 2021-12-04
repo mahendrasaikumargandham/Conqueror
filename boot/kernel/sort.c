582a031efbdcf927e3e71f3e60a9a42a6f6929b5
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2021-12-04 14:52:12 +0530

    Update sort.c

--- boot/kernel/sort.c
+++ boot/kernel/sort.c
@@ -228,7 +228,6 @@ async_safe_die (int errnum, char const *errstr)
   ignore_value (write (STDERR_FILENO, errstr, strlen (errstr)));
 
   /* Even if defined HAVE_STRERROR_R, we can't use it,
-     as it may return a translated string etc. and even if not
      For now just report the error number.  */
   if (errnum)
     {
