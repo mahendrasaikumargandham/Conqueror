41af84b65bdb29be4f83047a9822f99a91a884ab
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-01-01 09:36:23 +0530

    Create users.c

---
+++ boot/kernel/users.c
@@ -0,0 +1,92 @@
+#include <config.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include "system.h"
+#include "die.h"
+#include "error.h"
+#include "long-options.h"
+#include "quote.h"
+#include "readutmp.h"
+static int userid_compare (const void *v_a, const void *v_b)
+{
+  char **a = (char **) v_a;
+  char **b = (char **) v_b;
+  return strcmp (*a, *b);
+}
+static void list_entries_users (size_t n, const STRUCT_UTMP *this)
+{
+  char **u = xnmalloc (n, sizeof *u);
+  size_t i;
+  size_t n_entries = 0;
+  while (n--)
+    {
+      if (IS_USER_PROCESS (this))
+        {
+          char *trimmed_name;
+          trimmed_name = extract_trimmed_name (this);
+          u[n_entries] = trimmed_name;
+          ++n_entries;
+        }
+      this++;
+    }
+  qsort (u, n_entries, sizeof (u[0]), userid_compare);
+  for (i = 0; i < n_entries; i++)
+    {
+      char c = (i < n_entries - 1 ? ' ' : '\n');
+      fputs (u[i], stdout);
+      putchar (c);
+    }
+  for (i = 0; i < n_entries; i++)
+    free (u[i]);
+  free (u);
+}
+static void users (char const *filename, int options)
+{
+  size_t n_users;
+  STRUCT_UTMP *utmp_buf;
+  if (read_utmp (filename, &n_users, &utmp_buf, options) != 0)
+    die (EXIT_FAILURE, errno, "%s", quotef (filename));
+  list_entries_users (n_users, utmp_buf);
+  free (utmp_buf);
+}
+void usage (int status)
+{
+  if (status != EXIT_SUCCESS)
+    emit_try_help ();
+  else
+    {
+      printf (_("Usage: %s [OPTION]... [FILE]\n"), program_name);
+      printf (_("\
+Output who is currently logged in according to FILE.\n\
+If FILE is not specified, use %s.  %s as FILE is common.\n\
+\n\
+"), UTMP_FILE, WTMP_FILE);
+      fputs (HELP_OPTION_DESCRIPTION, stdout);
+      fputs (VERSION_OPTION_DESCRIPTION, stdout);
+      emit_ancillary_info (PROGRAM_NAME);
+    }
+  exit (status);
+}
+int main (int argc, char **argv)
+{
+  initialize_main (&argc, &argv);
+  set_program_name (argv[0]);
+  setlocale (LC_ALL, "");
+  bindtextdomain (PACKAGE, LOCALEDIR);
+  textdomain (PACKAGE);
+  atexit (close_stdout);
+  parse_gnu_standard_options_only (argc, argv, PROGRAM_NAME, PACKAGE_NAME, Version, true, usage, AUTHORS, (char const *) NULL);
+  switch (argc - optind)
+    {
+    case 0:			
+      users (UTMP_FILE, READ_UTMP_CHECK_PIDS);
+      break;
+    case 1:			
+      users (argv[optind], 0);
+      break;
+    default:			
+      error (0, 0, _("extra operand %s"), quote (argv[optind + 1]));
+      usage (EXIT_FAILURE);
+    }
+  return EXIT_SUCCESS;
+}
