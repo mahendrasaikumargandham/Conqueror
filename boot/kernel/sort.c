e1b2a7377a19daa74af2965a044b618ad1e56145
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-01-18 08:44:10 +0530

    Update sort.c

--- boot/kernel/sort.c
+++ boot/kernel/sort.c
@@ -769,9 +769,6 @@ stream_open (char const *file, char const *how)
   return fp;
 }
 
-/* Same as stream_open, except always return a non-null value; die on
-   failure.  */
-
 static FILE *
 xfopen (char const *file, char const *how)
 {
