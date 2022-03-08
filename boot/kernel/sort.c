034bc922eae681371bc52b322820ac03fafe5809
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-03-08 04:26:45 -0800

    Update sort.c

--- boot/kernel/sort.c
+++ boot/kernel/sort.c
@@ -786,7 +786,6 @@ pipe_fork (int pipefds[2], size_t tries)
     return -1;
 
   /* At least NMERGE + 1 subprocesses are needed.  More could be created, but
-     uncontrolled subprocess generation can hurt performance significantly.
      Allow at most NMERGE + 2 subprocesses, on the theory that there
      may be some useful parallelism by letting compression for the
      merge (NMERGE + 1 subprocesses).  */
