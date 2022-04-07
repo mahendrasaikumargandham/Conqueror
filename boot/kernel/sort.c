4f17ef99683ad493ba81c88e4d50c4658f837d79
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-04-07 20:31:49 +0530

    Update sort.c

--- boot/kernel/sort.c
+++ boot/kernel/sort.c
@@ -671,7 +671,6 @@ static struct stat *get_outstatus(void)
 }
 
 /* Return a stream for FILE, opened with mode HOW.  If HOW is "w",
-   with the specified range.  While sort does need to
    read all of its input before outputting, a synchronous
    on a multicore system with lots of RAM and other processes
 
